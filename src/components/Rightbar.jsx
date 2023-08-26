a0bd9710a443407877afb9525558a5da68352d34
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-26 10:00:56 +0100

    feat: hide sidebars on smaller screens

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,10 +1,11 @@
-import { Box } from "@mui/material"
-
+import { Box } from "@mui/material";
 
 const Rightbar = () => {
   return (
-    <Box flex={2} p={2}>Rightbar</Box>
-  )
-}
+    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      Rightbar
+    </Box>
+  );
+};
 
-export default Rightbar
+export default Rightbar;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,11 @@
-import { Box } from "@mui/material"
-
+import { Box } from "@mui/material";
 
 const Sidebar = () => {
   return (
-    <Box flex={1} p={2}>Sidebar</Box>
-  )
-}
+    <Box flex={1} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      Sidebar
+    </Box>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
