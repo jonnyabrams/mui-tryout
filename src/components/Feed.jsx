cb4bdbb7e373c520ec7767b80fded50abf5f56a1
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-26 14:48:46 +0100

    feat: Feed with posts

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,13 @@
-import { Box } from "@mui/material"
-
+import { Box } from "@mui/material";
+import Post from "./Post";
 
 const Feed = () => {
-  return (
-    <Box flex={4} p={2}>Feed</Box>
-  )
-}
+  return <Box flex={4} p={2}>
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+  </Box>;
+};
 
-export default Feed
+export default Feed;
