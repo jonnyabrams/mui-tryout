0fd8a4d5cf618dbb674c5c8d88af3d29d928b6a9
jonnyabrams <jonnyabrams@hotmail.com>
2023-08-26 10:26:37 +0100

    feat: navbar search & icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,32 @@
-import { EmojiEmotions } from "@mui/icons-material";
-import { AppBar, Toolbar, Typography, styled } from "@mui/material";
+import { EmojiEmotions, Mail, Notifications } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Toolbar,
+  Typography,
+  styled,
+} from "@mui/material";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "20px",
+  alignItems: "center",
+}));
 
 const Navbar = () => {
   return (
@@ -14,6 +36,21 @@ const Navbar = () => {
           JONNYBOOK
         </Typography>
         <EmojiEmotions sx={{ display: { sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={14} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
+          />
+        </Icons>
       </StyledToolbar>
     </AppBar>
   );
